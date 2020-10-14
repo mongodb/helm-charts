3fd46144e1b34e2f05a5cb45434d7c81c3e0cc50
theburi <belika@hotmail.com>
2020-10-14 14:42:35 +0100

    adding permission to exec file

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -6,8 +6,8 @@ KUBEVAL_VERSION="0.15.0"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
 # install kubeval
-# curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
-# tar -xf /tmp/kubeval.tar.gz kubeval
+curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
+tar -xf /tmp/kubeval.tar.gz kubeval
 
 # validate charts
 for CHART_DIR in ${CHART_DIRS}; do
