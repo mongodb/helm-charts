656181d63846899c06d50ab08a7d73b32f9b9a8e
Svetlana Maltseva <5437289+leo-ri@users.noreply.github.com>
2022-01-10 20:21:38 +0300

    add shellcheck, remove warnings, fix helper-tls script

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -5,12 +5,12 @@ set -uxo pipefail
 git remote add mongo https://github.com/mongodb/helm-charts.git
 git fetch mongo
 
-CHART_DIRS="$(git diff --find-renames --name-only $(git rev-parse --abbrev-ref HEAD) mongo/main -- charts | grep -i chart.yaml | xargs -r dirname)"
+CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" mongo/main -- charts | grep -i chart.yaml | xargs -r dirname)"
 KUBEVAL_VERSION="0.15.0"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
 # install kubeval
-curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
+curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz "https://github.com/instrumenta/kubeval/releases/download/${KUBEVAL_VERSION}/kubeval-linux-amd64.tar.gz"
 tar -xf /tmp/kubeval.tar.gz kubeval
 
 # validate charts
