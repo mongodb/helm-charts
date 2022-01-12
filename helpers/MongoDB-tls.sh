8531657540c034c1d3ae92ffa0585e2151d5fa6d
Svetlana <5437289+leo-ri@users.noreply.github.com>
2022-01-12 16:06:58 +0300

    CLOUDP-106100: add shellcheck (#105)

--- helpers/MongoDB-tls.sh
+++ helpers/MongoDB-tls.sh
@@ -3,19 +3,10 @@ set -euxo pipefail
 
 # Helper script to automate OpsManager Key creating
 
-# MongoDB name and namespace
-MONGODB_NAMESPACE=mongodb
+# MongoDB name
 MONGODB_NAME=mdbreplset
 
-# Information about Ops Manager deployment
-OPS_MANAGER_NAMESPACE=opsmanager
-OPS_MANAGER_HELM_RELEASE_NAME=opsmanager
-
-# Name of Ops Manager org where MongoDB will be dployed
-OPS_MANAGER_ORG_NAME=DemoOrg
-
-
-# Set up cert-manager: 
+# Set up cert-manager:
 # follow https://cert-manager.io/docs/installation
 
 # helm repo add jetstack https://charts.jetstack.io
@@ -24,26 +15,27 @@ OPS_MANAGER_ORG_NAME=DemoOrg
 # helm install cert-manager jetstack/cert-manager --namespace cert-manager --version v1.0.4 --set installCRDs=true --create-namespace
 
 # create Root certificate
-
-openssl genrsa -out ca.key 2048
-COMMON_NAME=*.svc.cluster.local
-openssl req -x509 -new -nodes -key ca.key -subj "/CN=${COMMON_NAME}" -days 3650 -reqexts v3_req -extensions v3_ca -out ca.crt
+cert_dir="cert-manager"
+openssl genrsa -out "$cert_dir/ca.key" 2048
+COMMON_NAME="*.svc.cluster.local"
+openssl req -x509 -new -nodes -key ca.key -subj "/CN=${COMMON_NAME}" -days 3650 -reqexts v3_req -extensions v3_ca -out "$cert_dir/ca.crt"
 
 # Deploy all nessesary and required certificates for MongoDB
-helm install certs .
+helm install certs ./cert-manager
 
 # postprocess issued certificates and build pem files
-for i in {0..2} 
-do 
-    kubectl get secret/${MONGODB_NAME}-${i} -o jsonpath='{.data.tls\.crt}' | base64 --decode > $MONGODB_NAME-${i}-pem
-    kubectl get secret/${MONGODB_NAME}-${i} -o jsonpath='{.data.tls\.key}' | base64 --decode >> $MONGODB_NAME-${i}-pem
+for i in {0..2}
+do
+    kubectl get "secret/${MONGODB_NAME}-${i}" -o jsonpath='{.data.tls\.crt}' | base64 --decode > "${MONGODB_NAME}-${i}-pem"
+    kubectl get "secret/${MONGODB_NAME}-${i}" -o jsonpath='{.data.tls\.key}' | base64 --decode >> "${MONGODB_NAME}-${i}-pem"
 done
 
 # Join all pem files into a secret that will be used by MongoDB
-kubectl create secret generic ${MONGODB_NAME}-cert $(for i in {0..2}; do echo --from-file=${MONGODB_NAME}-${i}-pem; done;)
+files_args=$(for i in {0..2}; do echo --from-file="${MONGODB_NAME}-${i}-pem"; done;)
+kubectl create secret generic "${MONGODB_NAME}-cert" "$files_args"
 
 rm ${MONGODB_NAME}-*
 
 # create mongoDB CA configmap
 cat ca.crt > ca-pem
-kubectl create configmap ${MONGODB_NAME}-ca --from-file=ca-pem
+kubectl create configmap "${MONGODB_NAME}-ca" --from-file=ca-pem
--- helpers/quickstart-atlas-cluster.sh
+++ helpers/quickstart-atlas-cluster.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-cluster_name=${1}
+cluster_name="${1}"
 ATLAS_PUBLIC_KEY=XXXX
 ATLAS_PRIVATE_KEY=YYYY
 ATLAS_ORG_ID=ZZZZ
-helm install $cluster_name \
+helm install "${cluster_name}" \
     ./charts/atlas-cluster \
-    --set atlas.publicApiKey=${ATLAS_PUBLIC_KEY} \
-    --set atlas.privateApiKey=${ATLAS_PRIVATE_KEY} \
-    --set atlas.orgId=${ATLAS_ORG_ID} \
-    --set project.name=${cluster_name} \
-    --set project.atlasProjectName=${cluster_name} \
-
+    --set atlas.publicApiKey="${ATLAS_PUBLIC_KEY}" \
+    --set atlas.privateApiKey="${ATLAS_PRIVATE_KEY}" \
+    --set atlas.orgId="${ATLAS_ORG_ID}" \
+    --set project.name="${cluster_name}" \
+    --set project.atlasProjectName="${cluster_name}" \
