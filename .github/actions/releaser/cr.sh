c7305cf7023a4f2242f572ba8c74ba0125226487
Jose Vazquez <jose.vazquez@mongodb.com>
2023-10-26 15:25:33 +0200

    remove comments
    Signed-off-by: Jose Vazquez <jose.vazquez@mongodb.com>

--- .github/actions/releaser/cr.sh
+++ .github/actions/releaser/cr.sh
@@ -59,13 +59,12 @@ release_charts_inside_folders() {
 
     # continue only with changed charts
     if [[ -n "${changed_charts[*]}" ]]; then
-        echo "would have released charts ${changed_charts[@]}"
-        # helm repo update
-        # install_chart_releaser
-        # cleanup_releaser
-        # package_charts "${changed_charts[@]}"
-        # release_charts
-        # update_index
+        helm repo update
+        install_chart_releaser
+        cleanup_releaser
+        package_charts "${changed_charts[@]}"
+        release_charts
+        update_index
     else
         echo "Nothing to do. No chart changes detected."
     fi
